db20a3b0d8944eba791cbc2053d7375d49a95b89
Alexandr Saleyev <saleyevav@yandex.ru>
2022-06-06 12:37:24 +0300

    add gradient

--- js/matrix-rain.js
+++ js/matrix-rain.js
@@ -3,6 +3,13 @@ const canvas = document.getElementById("canvas1");
 const ctx = canvas.getContext("2d");
 canvas.width = window.innerWidth;
 canvas.height = window.innerHeight;
+let gradient = ctx.createLinearGradient(0, 0, canvas.width, canvas.height);
+gradient.addColorStop(0, "red");
+gradient.addColorStop(0.2, "yellow");
+gradient.addColorStop(0.4, "green");
+gradient.addColorStop(0.6, "cyan");
+gradient.addColorStop(0.8, "blue");
+gradient.addColorStop(1, "magenta");
 
 function Symbol(x, y, fontSize, canvasHeight) {
   this.characters =
@@ -16,9 +23,9 @@ function Symbol(x, y, fontSize, canvasHeight) {
     this.text = this.characters.charAt(
       Math.floor(Math.random() * this.characters.length)
     );
-    context.fillStyle = "green";
+    context.fillStyle = gradient;
     context.fillText(this.text, this.x * this.fontSize, this.y * this.fontSize);
-    if (this.y * this.fontSize > this.canvasHeight && Math.random() > 0.9) {
+    if (this.y * this.fontSize > this.canvasHeight && Math.random() > 0.98) {
       this.y = 0;
     } else {
       this.y += 1;
@@ -38,16 +45,41 @@ function Effect(canvasWidth, canvasHeight) {
     }
   };
   this.init();
-  console.log(this.symbols);
+  this.resize = function (width, height) {
+    this.canvasWidth = width;
+    this.canvasHeight = height;
+    this.columns = this.canvasWidth / this.fontSize;
+    this.symbols = [];
+    this.init();
+  };
 }
 
 const effect = new Effect(canvas.width, canvas.height);
+let lastTime = 0;
+const fps = 15;
+const nextFrame = 1000 / fps;
+let timer = 0;
 
-function animate() {
-  ctx.fillStyle = "rgba(0, 0, 0, 0.05)";
-  ctx.fillRect(0, 0, canvas.width, canvas.height);
-  ctx.font = effect.fontSize + "px monospace";
-  effect.symbols.forEach((symbol) => symbol.draw(ctx));
+function animate(timeStamp) {
+  const deltaTime = timeStamp - lastTime;
+  lastTime = timeStamp;
+  if (timer > nextFrame) {
+    ctx.fillStyle = "rgba(0, 0, 0, 0.05)";
+    ctx.textAlign = "center";
+    ctx.fillRect(0, 0, canvas.width, canvas.height);
+    ctx.font = effect.fontSize + "px monospace";
+    effect.symbols.forEach((symbol) => symbol.draw(ctx));
+    timer = 0;
+  } else {
+    timer += deltaTime;
+  }
+
   requestAnimationFrame(animate);
 }
-animate();
+animate(0);
+
+window.addEventListener("resize", function () {
+  canvas.width = window.innerWidth;
+  canvas.height = window.innerHeight;
+  effect.resize(canvas.width, canvas.height);
+});
